35ad21d053052f991389eb0796c67e74b57878b5
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-07-24 23:44:00 +0300

    Hateoas removed
    small change

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -2,10 +2,8 @@ package com.ef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @SpringBootApplication
-@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class Parser {
 
     public static void main(String[] args) {
