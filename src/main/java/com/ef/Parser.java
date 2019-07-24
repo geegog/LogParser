f65525b00adbf89d54a04014e0289ae23a3c7fc4
Onuche Akor Idoko <gee_gog@yahoo.com>
2019-07-24 15:45:30 +0300

    Few updates
    java version downgrade from 12 to 8

--- src/main/java/com/ef/Parser.java
+++ src/main/java/com/ef/Parser.java
@@ -1,13 +1,13 @@
-package com.parser.demo;
+package com.ef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Parser {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Parser.class, args);
     }
 
 }
